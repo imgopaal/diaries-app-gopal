014b52d0084f240bf7a99fea45fa561ae1b72105
GopalDas123 <Gopaldax@gmail.com>
2021-04-04 00:59:37 +0500

    Setting up Redux Store

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupServer } from './services/mirage/server';
+
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
